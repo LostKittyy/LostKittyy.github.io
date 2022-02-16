2abf39b814d77ca0a4db75f97dd496b4f038bdf0
LostKittyy <92620338+LostKittyy@users.noreply.github.com>
2022-02-16 23:18:41 +0800

    Update wv21.js

--- wv21.js
+++ wv21.js
@@ -3,7 +3,7 @@ onmessage = function(e){
     let col = e.data[1]
     let d = e.data[2]
     let l = d.length / 2;
-    const xgap = -6;
+    const xgap = 6;
     const zgap = -6;
     let verts = [];
     let o = col*2+col-1;
@@ -22,9 +22,20 @@ onmessage = function(e){
         for (let j = 0; j < l; j++) {
             let x = d[j * 2]
             let z = d[j * 2 + 1]
-            verts = verts.concat([
+           /* verts = verts.concat([
               -1 + xgap * x + o, -1 + zgap*z+oz ,  -1 +oo, //0
                1 + xgap * x+o, -1 + zgap*z+oz,  -1+oo, //1
+              -1 + xgap * x+o,  1+zgap*z+oz,  -1+oo, //2
+               1 + xgap * x+o,  1+zgap*z+oz,  -1+oo, //3
+              -1 + xgap * x+o, -1+zgap*z+oz,   1+oo, //4
+               1 + xgap * x+o, -1+zgap*z+oz,   1+oo, //5
+              -1 + xgap * x+o,  1+zgap*z+oz,   1+oo, //6
+               1 + xgap * x+o,  1+zgap*z+oz,   1+oo,  //7
+            ])*/
+            
+            verts = verts.concat([
+              -1 + xgap* x, -1 + zgap*z+oz ,  -1 +oo, //0
+               1 + xgap * x+o, -1 + zgap*z+oz, -1+oo, //1
               -1 + xgap * x+o,  1+zgap*z+oz,  -1+oo, //2
                1 + xgap * x+o,  1+zgap*z+oz,  -1+oo, //3
               -1 + xgap * x+o, -1+zgap*z+oz,   1+oo, //4
